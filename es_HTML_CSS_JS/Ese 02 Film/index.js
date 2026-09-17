7b5980a76da659c98e33781c9d2fb9635d9aafc0
unknown <g.gaidano.5148@vallauri.net>
2026-09-17 11:19:35 +0200

    es 2 film

---
+++ es_HTML_CSS_JS/Ese 02 Film/index.js
@@ -0,0 +1,56 @@
+"use strict"
+
+const films = [
+    // Id, Title, Favorite, Watch date, Rating (0-5)
+    [1, "Pulp Fiction", true, "10-03-2024", 5],
+    [2, "21 Grammi", true, "17-03-2024", 3],
+    [3, "Star Wars", false, "15-03-2024", 1],
+    [4, "Matrix", false, "01-01-2023", 4],
+    [5, "Shrek", false, "21-03-2024", 2],
+    [6, "Kill Bill Vol. 1", true, "22-04-2024", 5],
+    [7, "Inception", true, "18-04-2024", 5]
+];
+
+
+let tBody = document.getElementsByTagName("tbody")[0];
+
+for (const film of films) {
+    let row = document.createElement("tr");
+    tBody.appendChild(row);
+    for (let i = 0; i< film.length; i++) {
+        const field = film[i]
+        let cell = document.createElement("td")
+        row.appendChild(cell)
+        if(i == 2){
+            //è il campo preferito
+            createPreferitoInnerHTML(cell, field)
+        }else if(i == 4){
+            // è il campo rating
+            createratingInnerHTML(cell, field)
+        }else{
+            //altro campo lo tratto come stringa
+            cell.innerHTML = field
+        }
+    }
+}
+
+function createPreferitoInnerHTML(cell, preferitovalue){
+    let check = document.createElement("input");
+    check.type = "checkbox";
+    check.disabled = true
+    check.checked = preferitovalue;
+    cell.appendChild(check);
+}
+
+function createratingInnerHTML(cell, ratingvalue){
+    for (let i = 0; i < 5; i++) {
+        let star = document.createElement("i")
+        if(i < ratingvalue){
+            star.classList.add("bi", "bi-star-fill");
+            
+        }else{
+            star.classList.add("bi", "bi-star");
+        }
+        cell.appendChild(star);
+    }
+}
